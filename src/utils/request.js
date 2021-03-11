4feae836e94057b0e6db2ae520d50900670f4b31
Ray <181250140@smail.nju.edu.cn>
2021-03-11 21:40:55 +0800

    add test

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import axios from 'axios'
 import { VueAxios } from './axios'
-import {notification, message} from 'ant-design-vue'
+import { notification, message } from 'ant-design-vue'
 import store from '@/store'
 import { getToken } from './auth'
 import router from '../router'
 
 // 创建 axios 实例
 const service = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '': 'http://localhost:8080',
+  baseURL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:8080',
   withCredentials: true
 })
 console.log(process.env.NODE_ENV)
- const err = (error) => { 
+const err = (error) => {
   if (error.response) {
     const data = error.response.data
     const token = Vue.ls.get('ACCESS_TOKEN')
@@ -37,13 +37,13 @@ console.log(process.env.NODE_ENV)
     }
   }
   return Promise.reject(error)
- }
+}
 
-//request incerceptor
+// request incerceptor
 service.interceptors.request.use((config) => {
   const requestConfig = {
     ...config,
-    url: `${config.url}`,
+    url: `${config.url}`
   }
   return requestConfig
 }, err)
@@ -51,7 +51,7 @@ service.interceptors.request.use((config) => {
 service.interceptors.response.use((response) => {
   switch (response.status) {
     case 200:
-      if(response.data.success && response.data.success){
+      if (response.data.success && response.data.success) {
         return response.data.content
       }
       message.error(response.data.message)
