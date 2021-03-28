501ea0a6377f6e42fae9557964d53942f4c2b3ac
181250098 <181250098@smail.nju.edu.cn>
2021-03-28 17:27:21 +0800

    add upload

--- src/home.vue
+++ src/home.vue
@@ -2,9 +2,31 @@
   <div style="height: 100%; width: 100%;">
     <CJS ref="ref_CJS"></CJS>
     <div class="buttonBar" style="background-color: rgb(220, 220, 220)">
-      <div class="button" style="margin-left: 80%">
+      <div class="button" style="margin-left: 70%">
+        <Button size="middle" style="background-color: #67758D;color: white; margin-right: 30px" @click="uploading" >上传文件</Button>
         <Button size="middle" style="background-color: #67758D;color: white; margin-right: 30px" @click="addNodes">添加节点</Button>
         <Button size="middle" style="background-color: #67758D;color: white" @click="delEles">删除</Button>
+      </div>
+      <div class="upload_form">
+        <a-modal
+          title="上传文件" :footer="null" :visible="uploadFormVisible" :confirmLoading="confirmLoading" @cancel="cancelUpload">
+          <a-upload-dragger name="file" ref="majorUpload" :multiple="true" :action="majorAction" :remove="handleRemove"
+                            :headers="majorHeaders" :before-upload="beforeUpload" @change="handleChange" :file-list="fileList">
+            <p class="ant-upload-drag-icon">
+              <a-icon style="color:#437FFF;font-size: 32px " type="vertical-align-top" />
+            </p>
+            <p class="ant-upload-text">
+              点击或将文件拖拽到这里上传
+            </p>
+            <p class="ant-upload-hint">
+              支持扩展名：.csv .json
+            </p>
+          </a-upload-dragger>
+          <!--      <a-upload-->
+          <!--        :fileList="fileList" :remove="handleRemove" show-upload-list="true" :beforeUpload="beforeUpload">-->
+          <!--      <a-button><a-icon type="upload" />选择文件</a-button>-->
+          <!--      </a-upload>-->
+        </a-modal>
       </div>
       <div class="change_form" >
         <a-modal :visible="addNodeFormVisible" title="增加节点" @cancel="cancelAddNode" @ok="addNode">
@@ -90,7 +112,10 @@ export default {
         }
       },
       nodeId: '',
-      all_property: []
+      all_property: [],
+      uploadFormVisible: false,
+      fileList: [],
+      confirmLoading: false
     }
   },
   methods: {
@@ -296,6 +321,86 @@ export default {
     cancelAddNodeProperty () {
       this.addNodePropertyFormVisible = false
       this.NodePropertyForm.resetFields()
+      this.all_property = []
+    },
+    // 点击上传文件跳出上传框
+    uploading () {
+      this.uploadFormVisible = true
+    },
+    // 点击取消上传
+    cancelUpload () {
+      this.uploadFormVisible = false
+    },
+    // 删除上传文件
+    handleRemove (file) {
+      const index = this.fileList.indexOf(file)
+      const newFileList = this.fileList.slice()
+      newFileList.splice(index, 1)
+      this.fileList = newFileList
+    },
+    // 显示上传文件内容
+    beforeUpload (file) {
+      this.spinning = true
+      var that = this
+      that.visible = false
+      // 文件类型
+      var fileName = file.name.substring(file.name.lastIndexOf('.') + 1)
+      if (fileName !== 'csv' && fileName !== 'json') {
+        this.$message.error('文件类型必须是csv或者json!')
+        this.spinning = false
+        that.visible = true
+        return false
+      }
+      return true
+    },
+    handleChange (info) {
+      console.log('239==', info)
+      const file = info.file
+      const name = file.name
+      const suffix = name.substr(name.lastIndexOf('.'))
+      if (suffix !== '.csv' || suffix !== '.json') {
+        return false
+      }
+      let fileList = [...info.fileList]
+      fileList = fileList.slice(-1)
+      fileList = fileList.map(file => {
+        if (file.response) {
+          file.url = file.response.url
+        }
+        return file
+      })
+      this.fileList = fileList
+      // console.log("this.fileList==",this.fileList);
+      if (this.fileList && this.fileList.length > 0 && this.fileList[0].response && this.fileList[0].response.code === 0) {
+        // this.fileList[0].response 执行的回调信息
+        const arr = this.fileList[0].response.list || []
+        if (arr.length > 0) {
+          let majorNames = ''
+          arr.forEach(item => {
+            if (majorNames) {
+              majorNames = majorNames + '，' + item.majorName
+            } else {
+              majorNames = item.majorName
+            }
+          })
+          this.$message.error('上传失败')
+        }
+        // console.log(this.$refs.majorUpload)
+        const suc = document.getElementsByClassName('ant-upload-list-item-name')
+        // console.log("274==",suc)
+        // let suc =  document.getElementsByClassName("ant-upload-list-item-card-actions");
+        if (suc && suc.length > 0) {
+          suc[0].style.width = '60%'
+          const parent = suc[0].parentNode
+          parent.style.display = 'flex'
+          parent.style.marginTop = '16px'
+          // console.log('278==parent==',parent)
+          const sp = document.createElement('span')
+          sp.innerText = '上传成功'
+          sp.style.color = '#437FFF'
+          parent.insertBefore(sp, suc[0].nextSibling)
+        }
+      }
       this.all_property = []
     }
   }
