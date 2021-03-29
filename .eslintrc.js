4366e53930eaaaa65a2f814f14625f2b23eda92f
Gabri <1034688982@qq.com>
2021-03-29 20:17:54 +0800

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,6 @@ module.exports = {
         "vue"
     ],
     "rules": {
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
       'no-undef': 'off',
       'vue/no-unused-vars': 'off',
       'vue/require-v-for-key': 'off',
